97f7770f0ed4b598fc7ca26e75e3cb037b391391
Lihao <usleolihao@gmail.com>
2020-03-08 19:02:43 -0700

    finished

--- Main_GUI.py
+++ Main_GUI.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import tkinter as tk # note that module name has changed from Tkinter in Python 2 to tkinter in Python 3
+import os.path
+
 from PlantsVsZombies.PlantsVsZombies import *
 from tkinter import messagebox
+from os import path
+from shutil import copyfile
 
 def get_platform():
 	platforms = {
@@ -47,6 +51,23 @@ class My_Cheat(tk.Frame):
 		self.window.mainloop()
 
 def main():
+	com38 = 'C:/Windows/System32/pythoncom38.dll'
+	wintype = 'C:/Windows/System32/pywintypes38.dll'
+	exist = path.exists(com38) and path.exists(wintype)
+	print ("PyWin32 File exists:", exist)
+	if not exist:
+		source1 = 'pywin32_system32/pythoncom38.dll'
+		source2 = 'pywin32_system32/pywintypes38.dll'
+		try:
+		    copyfile(source1, com38)
+		    copyfile(source2, wintype)
+		    print("Copied PyWin32 into C/Windows/System32")
+		except IOError as e:
+		    print("Unable to copy file. %s" % e)
+		    exit(1)
+		except:
+		    print("Unexpected error:", sys.exc_info())
+		    exit(1)
 	init_window = tk.Tk()
 	Cheat_plug = My_Cheat(init_window)
 	Cheat_plug.start()
